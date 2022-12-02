659541634968d00862657ec255460c3525ce91c1
Anthony Martinez <aenriquemartinez@gmail.com>
2022-12-01 23:40:15 -0500

    agregando jquery para envio del email

--- js/portafolio.js
+++ js/portafolio.js
@@ -12,3 +12,22 @@ function selecccionar(){
     document.getElementById("nav").classList = "";
     navmenu= false;
 }
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit',submit) 
+
+async function submit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action,{
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok) {
+        alert ('Gracias por conctactarme, le enviare una respuesta tan pronto como sea posible')
+    }
+}
